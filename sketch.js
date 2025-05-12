b25a9fbdf8013f7f45b93abd5ffeca874dac3229
Amanda <hello@amandahinton.com>
2025-05-11 22:44:21 -0400

    create shapes

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(800, 800); // width, height
 }
 
 function draw() {
@@ -9,21 +9,29 @@ function draw() {
 
   background(pink);
 
+  // x and y are coordinates of center
+  fill("white"); // call fill and stroke before shape
+  stroke("black");
+  strokeWeight(5);
+  rect(10, 10, 80, 160); // x y w h
+  ellipse(755, 715, 75, 150); // x y w h
+  square(10, 705, 80); // x y w
+  // xy pairs are coordinates of each vertex
+  triangle(790, 10, 790, 100, 710, 50); // x1 y1 x2 y2 x3 y3
+
   if (mouseIsPressed === true) {
     fill(coral);
   } else {
     fill("black");
   }
-
   noStroke();
-  circle(400, 400, 400); // x, y, width
+  circle(400, 400, 400); // x y w
 
   if (mouseIsPressed === true) {
     fill(lemon);
   } else {
     fill("white");
   }
-
   noStroke();
-  circle(mouseX, mouseY, 200); // x, y, width
+  circle(mouseX, mouseY, 200);
 }
